fed4bb7e699233f0baf80ed6e476dd2eca4f60d2
sidyea <115950981+sidyea@users.noreply.github.com>
2025-02-21 18:29:15 +0100

    First commit

--- pterodactyl/ContentView.swift
+++ pterodactyl/ContentView.swift
@@ -1,21 +1,61 @@
-//
-//  ContentView.swift
-//  pterodactyl
-//
-//  Created by Siddhant Dhanrajani on 21/02/2025.
-//
+
 
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+    
+    @State private var selected = "Baseball"
+    @State private var id = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            
+            Text("Why not try…")
+                .font(.largeTitle.bold())
+            
+            VStack {
+                Circle()
+                    .fill(colors.randomElement() ?? .blue)
+                    .padding()
+                    .overlay(
+                        Image(systemName: "figure.\(selected.lowercased())")
+                            .font(.system(size: 144))
+                            .foregroundColor(.white)
+                    )
+                
+                VStack (alignment: .leading){
+                    Text("\(selected)")
+                        .font(.title)
+                    .padding(.top)
+                    
+                    HStack {
+                        Text("Enjoy!")
+                            .font(.subheadline)
+                        
+                        Spacer()
+                        
+                        Text("\(id)")
+                            .font(.subheadline)
+                    }
+                }
+                .padding(.horizontal, 100)
+            }
+            .transition(.slide)
+            .id(id)
+            
+            Spacer()
+            
+            Button("Try again") {
+                withAnimation(.easeInOut(duration: 1)) {
+                    selected = activities.randomElement() ?? "Archery"
+                    id += 1
+                }
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .padding()
     }
 }
 
---
+++ pterodactyl/Posting.swift
@@ -0,0 +1,20 @@
+//
+//  Posting.swift
+//  pterodactyl
+//
+//  Created by Siddhant Dhanrajani on 21/02/2025.
+//
+
+import SwiftUI
+
+struct Posting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Posting_Previews: PreviewProvider {
+    static var previews: some View {
+        Posting()
+    }
+}
