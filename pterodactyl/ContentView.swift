ac4b2ea47136df53dafab1e7dba1beb03bb181e4
sidyea <115950981+sidyea@users.noreply.github.com>
2025-02-21 16:58:21 +0100

    Initial Commit

---
+++ pterodactyl/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  pterodactyl
+//
+//  Created by Siddhant Dhanrajani on 21/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pterodactyl/pterodactylApp.swift
@@ -0,0 +1,17 @@
+//
+//  pterodactylApp.swift
+//  pterodactyl
+//
+//  Created by Siddhant Dhanrajani on 21/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct pterodactylApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
